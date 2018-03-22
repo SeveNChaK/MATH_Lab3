54e6fa16ca1487f889fe53ca4340cf1f67e5b269
Danila Eremenko <danilaeremenko@mail.ru>
2018-03-22 03:26:27 +0300

    Оно работает

--- third/main.cpp
+++ third/main.cpp
@@ -3,30 +3,36 @@
 #include "../headers/rkf45.h"
 #include "../headers/euler.h"
 
-double h, bottom, up, y[2], tOut, error, errors;
-int iflag = 1;
+//Параметры
+double h, bottom, up, y[2], pointsNumber, tOut, error, errors, re = 1e-8, ae = 1e-8;
+int iflag = 1, n = 2;
+
+//Рабочие массивы
 double work[15];
+int iwork[30];
+double accurate[11];
+char line[] = "=========================================================================";
 
-int iwork[30], n = 2;
-double accurate[11], re = 1e-8, ae = 1e-8;
-
-char line[] = "=================================================================";
-
+//Полученная аналитическим методом систему двух ДУ 1-ого порядка
 void fun(double t, double *y, double *dy) {
     dy[0] = y[1];
-    dy[1] = -y[1] / 2 * t;
+    dy[1] = -y[1] / (2 * t);
 }
 
-double rightFun(double t) {
-    return 2*sqrt(t);
-}
-
-
-void setupDefaultTerms() {
+//Установка параметров
+void setup() {
     h = 0.1, bottom = 1, up = 2, tOut = bottom;
-    y[0] = 0, y[1] = 1, iflag = 1, errors = 0;
+    y[0] = 2, y[1] = 1, iflag = 1, errors = 0;
+    pointsNumber = (up - bottom) / h + 1;
+
 }
 
+//Точное решение
+double rightFun(double t) {
+    return 2 * sqrt(t);
+}
+
+//Функция, выводящая в консоль погрешность полученного решения, относительно точного решения
 void printEr(double tOut, int i) {
     error = abs(accurate[i] - y[0]);
     errors += error;
@@ -34,21 +40,27 @@ void printEr(double tOut, int i) {
 }
 
 int main() {
-    setupDefaultTerms();
-    printf(" h  \t%-20s%-20s%s\n", "RKF45", "Accurate", "Errors");
-    for (int i = 0; i < 11; ++i) {
-        tOut = i * h;
+    setup();
+    for (int i = 0; i < pointsNumber; ++i) {
+        tOut = i * h + 1;
         accurate[i] = rightFun(tOut);
+    }
+
+    //Считаем через RKF45
+    printf(" h  \t%-24s%-24s%s\n", "RKF45", "Accurate", "Errors");
+    for (int i = 0; i < pointsNumber; ++i) {
+        tOut = i * h + 1;
         RKF45(fun, n, y, &bottom, &tOut, &re, &ae, &iflag, work, iwork);
         printEr(tOut, i);
     }
     printf("\t\t%-40s%.15f\n%s\n", "Sum of errors:", errors, line);
 
 
-    setupDefaultTerms();
-    printf(" h  \t%-20s%-20s%s\n", "Euler-Cauchy", "Accurate", "Errors");
-    for (int i = 0; i < 11; ++i) {
-        tOut = i * h;
+    setup();
+    //Считаем методом Эйлера-Коши
+    printf(" h  \t%-24s%-24s%s\n", "Euler-Cauchy", "Accurate", "Errors");
+    for (int i = 0; i < pointsNumber; ++i) {
+        tOut = i * h + 1;
         euler(fun, n, h, y, tOut, iflag, EULER_CAUCHY);
         printEr(tOut, i);
     }
